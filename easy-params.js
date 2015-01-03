f04f8ecf767f276af73ed4d1d53229180cbca492
Daniel Perez <daniel@claudetech.com>
2015-01-03 20:11:47 +0100

    Improve naming.

--- easy-params.js
+++ easy-params.js
@@ -3,18 +3,18 @@
     var baseArgs = [];
     baseArgs.push.apply(baseArgs, arguments);
     var requiredArgsCount = baseArgs.shift();
-    var callee = baseArgs.pop();
+    var decorated = baseArgs.pop();
 
     return function () {
       if (arguments.length < requiredArgsCount) {
-        return callee.apply(this, arguments);
+        return decorated.apply(this, arguments);
       }
 
       var i;
       var args = [];
       args.push.apply(args, arguments);
 
-      var calleeArgs = [];
+      var decoratedArgs = [];
       var cb = null;
 
       if (typeof args[args.length - 1] === 'function') {
@@ -22,20 +22,20 @@
       }
 
       for (i = 0; i < requiredArgsCount; i++) {
-        calleeArgs.push(args.shift());
+        decoratedArgs.push(args.shift());
       }
 
       for (i = 0; i < baseArgs.length; i++) {
         if (args[i]) {
-          calleeArgs.push(args[i]);
+          decoratedArgs.push(args[i]);
         } else {
-          calleeArgs.push(baseArgs[i]);
+          decoratedArgs.push(baseArgs[i]);
         }
       }
 
-      calleeArgs.push(cb);
+      decoratedArgs.push(cb);
 
-      return callee.apply(this, calleeArgs);
+      return decorated.apply(this, decoratedArgs);
     };
   }
 
